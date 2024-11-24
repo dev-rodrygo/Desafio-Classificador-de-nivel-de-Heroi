0f62d492486dd5352df0438ea8d9c5eab5227a87
dev-rodrigo <rodrigo.off20@gmail.com>
2024-11-24 10:06:06 -0300

    Melhorando o desenpenho do codigo

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 const prompt = require('prompt-sync')()
+let NivelDoHeroi;
 
 for (let i = 0; i < 10; i++) {
     let heroi = prompt('Qual nome do herói?')
     let xp = prompt('Qual xp do heŕoi?')
     i++
     if (xp <= 1000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Ferro!`)
+        NivelDoHeroi = "Ferro"
     }
     else if (xp > 1001 && xp <= 2000) {
-        console.log(`O Herói de nome ${heroi} está no nível de bronze!`)
+        NivelDoHeroi = "Bronze"
 
     }
     else if (xp > 2000 && xp <= 5000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Prata!`)
+        NivelDoHeroi = "Prata"
 
     }
     else if (xp > 5000 && xp <= 7000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Ouro!`)
+        NivelDoHeroi = "Ouro"
 
     }
     else if (xp > 7000 && xp <= 8000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Platina!`)
+        NivelDoHeroi = "Platina"
 
     }
 
     else if (xp > 8000 && xp <= 9000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Ascendente!`)
+        NivelDoHeroi = "Ascendente"
 
     }
 
     else if (xp > 9000 && xp < 10000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Imortal!`)
-
+        NivelDoHeroi = "Imortal"
     }
     else if (xp > 1000) {
-        console.log(`O Herói de nome ${heroi} está no nível de Radiante!`)
-
+        NivelDoHeroi = "Radiante"
     }
+    console.log(`O Herói de nome ${heroi} está no nível de ${NivelDoHeroi}!`)
 }
+
+
